389d3efdb7763a38eb3670e76607d447f2396370
marovichn <nikolamarovic.11@gmail.com>
2023-06-08 20:47:56 +0200

    Adding-requests to db

---
+++ src/app/api/friends/add/route.ts
@@ -0,0 +1,63 @@
+import { fetchRedis } from "@helpers/redis";
+import { authOptions } from "@lib/auth";
+import { db } from "@lib/db";
+import { addFriendValidator } from "@lib/validation/add-friends";
+import { getServerSession } from "next-auth";
+import { z } from "zod";
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+
+    const { email: emailToAdd } = addFriendValidator.parse(body.email);
+
+    const RESTResponse = await fetch(
+      `${process.env.UPSTASH_REDIS_REST_URL}/get/user:email:${emailToAdd}`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.UPSTASH_REDIS_REST_TOKEN}`,
+        },
+        cache: "no-store",
+      }
+    );
+
+    const data = await RESTResponse.json() as {result: string};
+    const idToAdd = data.result;
+
+    if(!idToAdd){
+         return new Response("This person does not exist", { status: 400 });
+    }
+
+    const session = await getServerSession(authOptions);
+
+    if(!session){
+        return new Response("Unauthorized",{status: 401})
+    }
+
+    if(idToAdd === session.user.id){
+        return new Response("You can not add yourself!", { status: 400 });
+    }
+
+    const isAlreadyAdded =await fetchRedis("sismember", `user:${idToAdd}:incoming_friend_requests`, session.user.id) as 0 | 1;
+
+    if(isAlreadyAdded === 1){
+        return new Response("Already added this user", { status: 400 });
+    }
+    const isAlreadyFriends =await fetchRedis("sismember", `user:${session.user.id}:friends`, idToAdd) as 0 | 1;
+
+    if(isAlreadyFriends === 1){
+        return new Response("Already friends with this user", { status: 400 });
+    }
+    //valid request, send friend request
+
+    db.sadd(`user:${idToAdd}:incoming_friend_requests`, session.user.id);
+
+    return new Response("Success",{status: 200});
+
+  } catch (err) {
+    if(err instanceof z.ZodError){
+         return new Response("Invalid request payload", { status:422 });
+    }
+    return new Response("Invalid request", { status: 400 });
+  }
+}
--- src/components/AddFriendButton.tsx
+++ src/components/AddFriendButton.tsx
@@ -1,21 +1,77 @@
-"use client"
+"use client";
 
-import { FC } from 'react'
-import Button from './UI/Button'
+import { FC, useState } from "react";
+import Button from "./UI/Button";
+import { addFriendValidator } from "@lib/validation/add-friends";
+import axios from "axios";
+import { AxiosError } from "axios";
+import { toast } from "react-hot-toast";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-interface AddFriendButtonProps {
-  
-}
+interface AddFriendButtonProps {}
 
 const AddFriendButton: FC<AddFriendButtonProps> = ({}) => {
-  return <form className='max-w-sm'>
-<label htmlFor='email' className='block text-sm font-medium leading-6 text-gray-900'>Add friend by E-mail</label>
+  const [showSuccesssState, setShowSuccessState] = useState<boolean>(false);
 
-<div className='mt-2 flex gap-4'>
-    <input type="text" className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6' placeholder='you@example.com'/>
-    <Button>Add</Button>
-</div>
-  </form>
-}
+  type FormData = z.infer<typeof addFriendValidator>
 
-export default AddFriendButton
+  const {
+    register, handleSubmit, setError, formState:{errors}
+  } = useForm<FormData>({
+    resolver: zodResolver(addFriendValidator),
+  });
+
+  const addFriend = async (email: string) => {
+    try {
+      const validatedEmail = addFriendValidator.parse({ email });
+
+      await axios.post("/api/friends/add", {
+        email: validatedEmail,
+      });
+
+      setShowSuccessState(true);
+    } catch (err) {
+      if (err instanceof z.ZodError) {
+        setError("email", {message: err.message});
+        return;
+      }
+      if (err instanceof AxiosError) {
+        setError("email", { message: err.response?.data });
+        return;
+      }
+
+      setError("email", {message: "Something wnt wrong"});
+    }
+  };
+
+  const onSubmit = (data: FormData)=>{
+    addFriend(data.email)
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}className='max-w-sm'>
+      <label
+        htmlFor='email'
+        className='block text-sm font-medium leading-6 text-gray-900'
+      >
+        Add friend by E-mail
+      </label>
+
+      <div className='mt-2 flex gap-4'>
+        <input
+        {...register("email")}
+          type='text'
+          className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+          placeholder='you@example.com'
+        />
+        <Button>Add</Button>
+      </div>
+      <p className="mt-1 text-sm text-red-600">{errors.email?.message}</p>
+      {showSuccesssState ? <p className="mt-1 text-sm text-green-600">Friend request sent!</p> : null}
+    </form>
+  );
+};
+
+export default AddFriendButton;
