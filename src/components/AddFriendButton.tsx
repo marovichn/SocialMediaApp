31789836e4f1c0d2345874dd283da0741a0fe717
marovichn <nikolamarovic.11@gmail.com>
2023-06-08 18:14:29 +0200

    Adding friends page

---
+++ src/components/AddFriendButton.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import { FC } from 'react'
+
+interface AddFriendButtonProps {
+  
+}
+
+const AddFriendButton: FC<AddFriendButtonProps> = ({}) => {
+  return <div>AddFriendButton</div>
+}
+
+export default AddFriendButton
