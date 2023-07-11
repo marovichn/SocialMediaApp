3f57ffe24438b53ab43f73f1955b8a9778d6efc2
marovichn <nikolamarovic.11@gmail.com>
2023-07-11 14:48:22 +0200

    Friends-data-sidebar-display-test

--- src/components/SideBarChatList.tsx
+++ src/components/SideBarChatList.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { FC } from "react";
 
 interface SideBarChatListProps {
@@ -8,7 +9,21 @@ const SideBarChatList: FC<SideBarChatListProps> = ({ friends }) => {
   return (
     <ul className='max-h-[25rem] overflow-y-auto -mx-2 space-y-1' role='list'>
       {friends.sort().map((friend) => {
-        return <li>{friend.name}</li>;
+        return (
+          <li className='flex gap-3 align-self bg-neutral-100 rounded-md p-4'>
+            <Image
+              className='rounded-full w-9 h-9 m-2'
+              src={friend.image}
+              alt='user_image'
+              width={20}
+              height={20}
+            />
+            <div className="flex flex-col">
+              {friend.name}
+              <div>{friend.email}</div>
+            </div>
+          </li>
+        );
       })}
     </ul>
   );
