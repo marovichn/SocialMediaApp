ffc89c3198993107d6c202dfddc1b4ab62730df0
marovichn <nikolamarovic.11@gmail.com>
2023-07-13 21:51:11 +0200

    Realtime-messages

--- src/app/(dashoard)/dashboard/layout.tsx
+++ src/app/(dashoard)/dashboard/layout.tsx
@@ -38,7 +38,7 @@ const layout = async ({ children }: LayoutProps) => {
 
   return (
     <div className='w-full flex h-screen'>
-      <div className='flex h-full w-full max-w-sm grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6'>
+      <div className='flex h-full max-w-sm grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6 w-full'>
         <Link href='/dashboard' className='flex h-16 shrink-0 items-center'>
           <Icons.Logo className='h-8 w-auto text-lime-600' />
         </Link>
@@ -51,10 +51,10 @@ const layout = async ({ children }: LayoutProps) => {
             Add some friends
           </div>
         )}
-        <nav className='flex flex-1 flex-col'>
+        <nav className='flex flex-1 flex-col  md:max-w-[1rem]'>
           <ul role='list' className='flex flex-1 flex-col gap-y-7'>
             <li>
-              <SideBarChatList friends={friends} sessionId={session.user.id}/>
+              <SideBarChatList friends={friends} sessionId={session.user.id} />
             </li>
             <li>
               <div className='text-xs font-semibold leading-6 text-gray-400'>
@@ -96,7 +96,7 @@ const layout = async ({ children }: LayoutProps) => {
               <div className='flex flex-1 items-center gap-x-4 px-6 py-3 text-sm font-semibold leading-6 text-gray-900'>
                 <div className='relative h-8 w-8 bg-gray-50 rounded-full'>
                   <Image
-                  sizes="1"
+                    sizes='1'
                     fill
                     referrerPolicy='no-referrer'
                     className='rounded-full'
@@ -119,7 +119,7 @@ const layout = async ({ children }: LayoutProps) => {
           </ul>
         </nav>
       </div>
-      {children}
+      <aside className='max-h-screen container w.full'>{children}</aside>
     </div>
   );
 };
--- src/app/api/friends/accept/route.ts
+++ src/app/api/friends/accept/route.ts
@@ -1,6 +1,8 @@
 import { fetchRedis } from "@helpers/redis";
 import { authOptions } from "@lib/auth";
 import { db } from "@lib/db";
+import { pusherServer } from "@lib/pusher";
+import { toPusherKey } from "@lib/utils";
 import { getServerSession } from "next-auth";
 import { z } from "zod";
 
@@ -33,6 +35,8 @@ export async function POST(req: Request) {
     if (!hasFriendRequest) {
       return new Response("This request is invalid", { status: 400 });
     }
+
+    const sender = JSON.parse(await db.get(`user:${idToAdd}`) as string)as User;
 
     await db.sadd(`user:${session.user.id}:friends`, idToAdd);
 
