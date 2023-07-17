b16e793032c83c72e2eda951c11538dc29fd3779
marovichn <nikolamarovic.11@gmail.com>
2023-07-17 17:13:53 +0200

    Loading-states-UX-messages

---
+++ src/app/(dashoard)/dashboard/requests/loading.tsx
@@ -0,0 +1,16 @@
+import { FC } from "react";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+
+const loading: FC = ({}) => {
+  return (
+    <div className='w-fill flex flex-col gap-3 p-15 '>
+      <Skeleton className='mt-10 ml-10 mb-8' height={60} width={500} />
+      <Skeleton className='ml-10' height={50} width={350} />
+      <Skeleton className='ml-10' height={50} width={350} />
+      <Skeleton className='ml-10' height={50} width={350} />
+    </div>
+  );
+};
+
+export default loading;
