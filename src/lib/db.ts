8b8ae6e19df9ebe6a02cf0e5c192a2aa2afde384
marovichn <nikolamarovic.11@gmail.com>
2023-07-21 23:10:10 +0200

    Credibility-fix

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -3,5 +3,5 @@ import { Redis } from "@upstash/redis";
 export const db = new Redis({
   url: "https://inspired-dingo-37040.upstash.io",
   token:
-    "AZCwASQgZTA0MWU2MDAtYjUyMS00ODNiLWFhZWMtOWZhNzUwOTMzYTRmMzI0MWE0ODA2OTM4NDJhN2EwZTM4ZDlkYzgzNTNkYTg=",
+    "AZCwASQgZTA0MWU2MDAtYjUyMS00ODNiLWFhZWMtOWZhNzUwOTMzYTRmZGIzMzVmODk2OTMwNGQ2NjkzZmFmZTFkNDJlODljYjU=",
 });
