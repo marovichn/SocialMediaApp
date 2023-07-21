6d54861001ec583a0b1b1bd1a010b621dd51f097
marovichn <nikolamarovic.11@gmail.com>
2023-07-21 17:42:52 +0200

    build-v.1.1.2

--- src/app/api/friends/accept/route.ts
+++ src/app/api/friends/accept/route.ts
@@ -35,41 +35,30 @@ export async function POST(req: Request) {
     if (!hasFriendRequest) {
       return new Response("This request is invalid", { status: 400 });
     }
-    const acceptor = JSON.parse(
-      (await fetchRedis("get", `user:${session.user.id}`)) as string
-    ) as User;
+    const acceptor = JSON.parse(await fetchRedis("get", `user:${session.user.id}`) as string)as User;
     const sender = JSON.parse(
       (await fetchRedis("get", `user:${idToAdd}`)) as string
     ) as User;
 
-    await Promise.all([
-      pusherServer.trigger(
-        toPusherKey(`user:${idToAdd}:friends`),
-        "new_friend",
-        [acceptor, sender]
-      ),
+    pusherServer.trigger(toPusherKey(`user:${idToAdd}:friends`),"new_friend",[acceptor, sender]);
+    
+    pusherServer.trigger(toPusherKey(`user:${session.user.id}:friends`),"new_friend",[acceptor, sender]);
 
-      pusherServer.trigger(
-        toPusherKey(`user:${session.user.id}:friends`),
-        "new_friend",
-        [acceptor, sender]
-      ),
+    await db.sadd(`user:${session.user.id}:friends`, idToAdd);
 
-      db.sadd(`user:${session.user.id}:friends`, idToAdd),
+    await db.sadd(`user:${idToAdd}:friends`, session.user.id);
 
-      db.sadd(`user:${idToAdd}:friends`, session.user.id),
+    await db.srem(`user:${idToAdd}:incoming_friend_requests`, session.user.id);
 
-      db.srem(`user:${idToAdd}:incoming_friend_requests`, session.user.id),
+    await db.srem(`user:${session.user.id}:incoming_friend_requests`, idToAdd);
 
-      db.srem(`user:${session.user.id}:incoming_friend_requests`, idToAdd),
-    ]);
 
     return new Response("Success", { status: 200 });
   } catch (err) {
-    if (err instanceof z.ZodError) {
-      return new Response("Invalid request payload", { status: 422 });
+    if(err instanceof z.ZodError){
+      return new Response("Invalid request payload", {status: 422})
     }
 
-    return new Response("Invalid request", { status: 400 });
+    return new Response("Invalid request", {status: 400})
   }
 }
